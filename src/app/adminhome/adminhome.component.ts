7ab1354e64df99b6e1873a3cb8fa68d6a1dcfc0b
patilpratikha <pratikshanila76@gmail.com>
2024-07-12 20:35:53 +0530

    to remove dashoard once clicked

--- src/app/adminhome/adminhome.component.ts
+++ src/app/adminhome/adminhome.component.ts
@@ -13,24 +13,31 @@ import { User } from '../model/User';
 export class AdminhomeComponent implements OnInit {
   public users: any[];
   errorMessage: string;
+  isOn: boolean; 
   constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.isOn=true;
   }
 
   bookings(){
+    this.isOn=false;
     this.router.navigate(['/booking']);
   }
   planes(){
+    this.isOn=false;
     this.router.navigate(['/plane']);
   }
   routes(){
+    this.isOn=false;
     this.router.navigate(['/route']);
   }
   schedules(){
+    this.isOn=false;
     this.router.navigate(['/schedule']);
   }
   getUsers(){
+    this.isOn=false;
     this.userService.getAllUserDetails().subscribe(
       (data: any) => {
         this.users = data;
