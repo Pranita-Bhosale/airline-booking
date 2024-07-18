9b6cbf4abdc5bd160d48ba7b5e9dda6d16d4a9a6
Pranita-Bhosale <pranitabhosale192@gmail.com>
2024-07-18 10:02:33 +0530

    updated user service file

--- src/app/adminhome/adminhome.component.ts
+++ src/app/adminhome/adminhome.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-adminhome',
   templateUrl: './adminhome.component.html',
@@ -8,7 +10,9 @@ import { Router } from '@angular/router';
 })
 export class AdminhomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+
+
+  constructor(private router: Router, ) { }
 
   ngOnInit(): void {
   }
